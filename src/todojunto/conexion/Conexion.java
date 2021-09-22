fed50e9ec275d383414396fbbc65b901e2210feb
alumno-curioso <devscubriendo@gmail.com>
2021-09-22 07:53:41 +0000

    probando el GUI de intelliJey destinado a la creacion de interfases de usuario con SWING unos pasos basicos con la herramienta https://www.youtube.com/watch?v=aIdIXsi1qTU

--- src/todojunto/conexion/Conexion.java
+++ src/todojunto/conexion/Conexion.java
@@ -1,22 +1,22 @@
-package todojunto.conexion;
-
-import javax.swing.*;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class Conexion {
-    private void ClaseDatos() {
-        try {
-
-            //Class.forName("com.mysql.jdbc.Driver"); esta es la otra opción para crear una conexión.
-//            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            String sUrl = "jdbc:mysql://localhost:3306/libros";
-            conn = DriverManager.getConnection(sUrl, "root", "1234");
-            JOptionPane.showMessageDialog(null, "Conectado!!!!");
-
-        } catch (SQLException ex) {
-            conn = null;
-            throw new RuntimeException("Error con la conexión!!!");
-        }
-    }
-}
+//package todojunto.conexion;
+//
+//import javax.swing.*;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//public class Conexion {
+//    private void ClaseDatos() {
+//        try {
+//
+//            //Class.forName("com.mysql.jdbc.Driver"); esta es la otra opción para crear una conexión.
+////            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+//            String sUrl = "jdbc:mysql://localhost:3306/libros";
+//            conn = DriverManager.getConnection(sUrl, "root", "1234");
+//            JOptionPane.showMessageDialog(null, "Conectado!!!!");
+//
+//        } catch (SQLException ex) {
+//            conn = null;
+//            throw new RuntimeException("Error con la conexión!!!");
+//        }
+//    }
+//}
--- src/view/vistaPrincipal.java
+++ src/view/vistaPrincipal.java
@@ -1,7 +1,30 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class vistaPrincipal {
+public class vistaPrincipal extends JFrame {
     private JPanel panel1;
+    private JButton button1;
+
+
+    public vistaPrincipal() {
+        setTitle("holamundoGUI");
+        setSize(400,400);
+        setLocationRelativeTo(null);
+
+        add(panel1);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("hi world!!!");
+            }
+        });
+    }
 }
