c0e96f651af6379307981381244b45b1bb00354e
alumno-curioso <devscubriendo@gmail.com>
2021-09-23 10:46:22 +0000

    creamos la vista principal de la aplicacion, solo son 4 botones y un label para luego hacer las vistas de cada una de las opciones

--- src/server/querys/PredefinedQuerys.java
+++ src/server/querys/PredefinedQuerys.java
@@ -2,4 +2,5 @@ package server.querys;
 
 public class PredefinedQuerys {
     final static String CHECK_USER ="select * from usuarios where usuario=? and clave=?";
+    final static String ALL_STUDENTS = "select * from usuarios";
 }
---
+++ src/view/MainView.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.*;
+
+public class MainView {
+    private JPanel jpMainView;
+    private JLabel lbMainView;
+    private JButton jbStudent;
+    private JButton jbArchive;
+    private JButton jbUsers;
+    private JButton jbExit;
+}
