ed8497bcf6be9c90502b4198a4ea6d898f10d85c
alumno-curioso <devscubriendo@gmail.com>
2021-09-24 09:49:14 +0000

    modificamos los los contructores de las vistas recien creadas y aniadimos el panel respectivo a sus frames

--- src/view/LoanView.java
+++ src/view/LoanView.java
@@ -3,20 +3,24 @@ package view;
 import javax.swing.*;
 
 public class LoanView  extends JFrame{
+    private JPanel jpLoanView;
     private JTextField tfDNIStudent;
     private JLabel lbDNIStudentd;
     private JTextField tfIDBook;
     private JLabel lbIDBook;
     private JButton jbLoan;
-    private JPanel jpLoanView;
     private JLabel lbLoanView;
     private JButton jbReturn;
     private JButton jbReturnMainView;
+    private JLabel jlDNIStudent;
+    private JTextField jtDNIStudent;
 
     public LoanView(){
 
         setSize(DefaultParameters.WINDOWS_SIZE);
         setLocationRelativeTo(null);
+
+        add(jpLoanView);
 
 
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
--- src/view/StudentView.java
+++ src/view/StudentView.java
@@ -1,2 +1,24 @@
-package view;public class StudentView {
+package view;
+
+import javax.swing.*;
+
+public class StudentView extends JFrame {
+    private JPanel jpStudentView;
+    private JButton jbLoan;
+    private JButton jcCreateStudent;
+    private JButton jbUpdateStudent;
+    private JButton jbReadStudent;
+    private JButton jbDeleteStudent;
+    private JLabel jlDNI;
+    private JLabel jlName;
+    private JTextField textField1;
+
+    public StudentView(){
+        setSize(DefaultParameters.WINDOWS_SIZE);
+        setLocationRelativeTo(null);
+
+        add(jpStudentView);
+
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+    }
 }
