4bd2ced53531a8bc38c98b8771f08fea96de7117
alumno-curioso <devscubriendo@gmail.com>
2021-09-26 10:46:23 +0000

    dejamos preparado el controlador de prestamos

--- src/controller/LoanController.java
+++ src/controller/LoanController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import view.LoanView;
+
 public class LoanController {
+    LoanView loanView;
+
+    public LoanController(){
+        loanView = new LoanView();
+
+        loanView.getJbLoan().addActionListener(e->{
+            //todo crear metodo de prestamos
+        });
+        loanView.getJbLoanReturn().addActionListener(e->{
+            //todo crear metodo de devoluciones
+        });
+        loanView.getJbReturnMainView().addActionListener(e->{
+            //todo volver al menu pricipal
+        });
+    }
 }
