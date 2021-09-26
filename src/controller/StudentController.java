9991d50772818e1a0fbb2b098afec3695ad7a446
alumno-curioso <devscubriendo@gmail.com>
2021-09-26 10:43:16 +0000

    dejamos preparada la clase controlador de alumnos

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -1,4 +1,29 @@
 package controller;
 
+import view.StudentView;
+
 public class StudentController {
+
+    StudentView studentView;
+
+    public StudentController(){
+        studentView = new StudentView();
+
+        studentView.getJcCreateStudent().addActionListener(e->{
+            //todo crear estudiante
+        });
+        studentView.getJbReadStudent().addActionListener(e->{
+            //todo read estudiante
+        });
+        studentView.getJbUpdateStudent().addActionListener(e->{
+            //todo update studiante
+        });
+        studentView.getJbDeleteStudent().addActionListener(e->{
+            //todo delete student
+        });
+        studentView.getJbReturnMainView().addActionListener(e->{
+            //todo volver a pagina  principal
+        });
+    }
+
 }
